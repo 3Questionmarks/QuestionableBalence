c2df4192a4896507a52be1973f6b35c0f5711d0b
3Questionmarks <thetripplequestionmark@gmail.com>
2024-03-12 19:45:49 +0000

    Removed a mod I was steeling assets from

--- +More Faith Buildings+ (v 530)/FaithBuildings_DBMaster.sql
+++
@@ -1,3 +0,0 @@
--- Sapiens_DBUpdates
-CREATE TABLE IF NOT EXISTS Sapiens_DBUpdates (Name TEXT PRIMARY KEY, Value INTEGER DEFAULT 0);
-INSERT INTO Sapiens_DBUpdates (Name) VALUES ('FaithB_ArtInfo'),('FaithB_Balance'),('FaithB_Defines');
